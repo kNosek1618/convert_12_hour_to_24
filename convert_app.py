b328e5834b437b9b151abbaf6d772124a1ec28ac
kNosek1618 <k.nosek@ievent-management.pl>
2019-06-19 08:43:01 +0200

    add coments

--- convert_app.py
+++ convert_app.py
@@ -1,17 +1,21 @@
 
-
+# convert time from 12 hour to 24 hour format
 def convert(str1):
+    #checking if AM and 12 format
     if str1[-2:] == "AM" and str1[:2] == "12":
         return "00" + str1[2:-2]
 
+    # remove the AM
     elif str1[-2:] == "AM":
         return str1[:-2]
 
+    # checking if PM and 12 format
     elif str1[-2:] == "PM" and str1[:2] == "12":
         return str1[:-2]
 
     else:
 
+        # add 12 hours to hours and remove PM
         return str(int(str1[:2]) + 12) + str1[2:8]
 
 print(convert("08:05:45 PM"))
